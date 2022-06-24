daa24e73f0df74dfcbee8aa1db606650b8e101ab
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-06-24 19:03:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ terraform {
     }
   }
 }
+
+provider "oci" {
+  tenancy_ocid = var.tenancy_ocid
+  region = var.region
+  
+  user_ocid = var.user_ocid
+  private_key = var.private_key
+  fingerprint = var.fingerprint
+}
